49948a5fd0e9fded3faf51e48b655618a71bdda5
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2025-06-09 17:29:34 +0100

    Update tests and functions

--- main.js
+++ main.js
@@ -7,7 +7,88 @@ function reverseString(string) {
   return string.split("").reverse().join("");
 }
 
-// console.log(capitalize("a thing OF WOundS"));
+const calculator = {
+  add: function (x, y) {
+    return x + y;
+  },
+  subtract: function (x, y) {
+    return x - y;
+  },
+  divide: function (x, y) {
+    return x / y;
+  },
+  multiply: function (x, y) {
+    return x * y;
+  },
+};
 
-module.exports = capitalize;
-module.exports = reverseString;
+function caesarCipher(str, shift) {
+  const lower = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+  const upper = lower.join().toUpperCase().split(",");
+  let newStr = "";
+  str.split("").forEach((item) => {
+    if (lower.includes(item)) {
+      if (lower.indexOf(item) + shift <= 25) {
+        newStr += lower[lower.indexOf(item) + shift];
+      } else {
+        newStr += lower[lower.indexOf(item) + shift - 26];
+      }
+    } else if (upper.includes(item)) {
+      if (upper.indexOf(item) + shift <= 25) {
+        newStr += upper[upper.indexOf(item) + shift];
+      } else {
+        newStr += upper[upper.indexOf(item) + shift - 26];
+      }
+    } else {
+      newStr += item;
+    }
+  });
+  return newStr;
+}
+
+function analyzeArray(array) {
+  return {
+    average:
+      array.reduce((previous, current) => previous + current) / array.length,
+    min: array.sort((a, b) => a - b)[0],
+    max: array.sort((a, b) => a - b)[array.length - 1],
+    length: array.length,
+  };
+}
+
+console.log(analyzeArray([1, 8, 3, 4, 2, 6]));
+
+module.exports = {
+  capitalize,
+  reverseString,
+  calculator,
+  caesarCipher,
+  analyzeArray,
+};
--- main.test.js
+++ main.test.js
@@ -1,5 +1,10 @@
-const capitalize = require("./main");
-const reverseString = require("./main");
+const {
+  capitalize,
+  reverseString,
+  calculator,
+  caesarCipher,
+  analyzeArray,
+} = require("./main");
 
 test("Capitalizes only the first character in a string", () => {
   expect(capitalize("a thing OF WOundS")).toBe("A thing of wounds");
@@ -7,4 +12,45 @@ test("Capitalizes only the first character in a string", () => {
 
 test("Reverses a string", () => {
   expect(reverseString("wounds")).toBe("sdnuow");
+});
+
+test("Adds two numbers", () => {
+  expect(calculator.add(2, 3)).toBe(5);
+});
+
+test("Subtracts a number from another", () => {
+  expect(calculator.subtract(2, 3)).toBe(-1);
+});
+
+test("Divide a number by another", () => {
+  expect(calculator.divide(3, 2)).toBe(1.5);
+});
+
+test("Multiply two numbers", () => {
+  expect(calculator.multiply(2, 3)).toBe(6);
+});
+
+test("Returns a new string in which each character of a given string is shifted by some factor", () => {
+  expect(caesarCipher("abc", 3)).toBe("def");
+});
+
+test("Wrap from z to a", () => {
+  expect(caesarCipher("xyz", 3)).toBe("abc");
+});
+
+test("caesarCipher should be case-sensitive", () => {
+  expect(caesarCipher("HeLLo", 3)).toBe("KhOOr");
+});
+
+test("caesarCipher should be preserve punctuations, spaces and numbers", () => {
+  expect(caesarCipher("Hello, World! 6", 3)).toBe("Khoor, Zruog! 6");
+});
+
+test("Returns an object that gives the average, min and max values in an array, and the length of the array", () => {
+  expect(analyzeArray([1, 8, 3, 4, 2, 6])).toStrictEqual({
+    average: 4,
+    min: 1,
+    max: 8,
+    length: 6,
+  });
 });
